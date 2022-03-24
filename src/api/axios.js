f89ce1c32a95589608fd9051660dd13eec836ade
Amine ZEGMOU <63230216+ZeAmine@users.noreply.github.com>
2022-03-25 00:44:01 +0100

    ☁️

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://h3-proxy.services.quickpipes.io/team2/'
+  baseURL: 'http://localhost:8080'
 })
+
+  // baseURL: 'https://h3-proxy.services.quickpipes.io/team2/'
